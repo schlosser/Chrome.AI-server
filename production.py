2dd594ad314b5ab1f384e518ce7d6ef2258bcc2e
Ben Edelstein <ben.edelstein93@gmail.com>
2015-06-27 11:46:54 -0700

    fix for heroku

--- production.py
+++ production.py
@@ -2,9 +2,9 @@ from app import create_app
 import routes
 import os
 
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
 
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
