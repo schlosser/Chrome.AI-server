b6109cc2d889b75a7ea6d9e0a051d41d75c040de
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-06-27 14:56:36 -0700

    error handling

--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, request, BadRequest
+from flask import Blueprint, request
+from werkzeug.exceptions import BadRequest
 from util.json_response import json_success, json_error_message
 from util.wit_helper import get_all_intents, get_all_entities
 from util.train import train
 
 api = Blueprint('api', __name__)
+NO_JSON = "Found no JSON.  Did you set Content-Type: application/json?"
 
 
 @api.route('/')
@@ -15,9 +17,13 @@ def hello_world():
 def new_training_data():
     try:
         training_data = request.get_json()
+        if training_data is None:
+            raise BadRequest(description=NO_JSON)
         train(training_data)
-    except BadRequest:
-        return json_error_message('Failed to parse JSON data')
+    except BadRequest as e:
+        print e, e.description
+        return json_error_message('Failed to parse JSON data',
+                                  error_data=e.description)
 
     return json_success('Training data added successfully')
 
